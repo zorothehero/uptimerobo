b296a8e42c9104cb253c26158f1b16befde45f7b
zorothehero <49325617+zorothehero@users.noreply.github.com>
2020-06-20 15:47:06 +0200

    Update url.js

--- url.js
+++ url.js
@@ -1,13 +1,7 @@
 const urls = [
-    "http://aurora-discord-bot-hehe.glitch.me/ping",
-    "http://ecobot-inex.glitch.me",
-    "http://pikabot-discord.glitch.me",
-    "http://giveawayboat-discord.glitch.me",
-    "http://snowboats.glitch.me",
-    "http://elixirbot.glitch.me",
-    "http://bestest-bot.glitch.me",
-    "http://zyrouge-bot-list.glitch.me/ping",
-    "https://api.zyrouge.gq"
+
+    "https://managerdiscord.glitch.me"
+
 ];
 
 module.exports = urls;
